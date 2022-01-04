09c965f6383fb5b9e09db33b45bf32cfc4a706e2
yihong0618 <zouzou0208@gmail.com>
2022-01-04 17:10:34 +0800

    fix: no gtd label

--- daily/utils.py
+++ daily/utils.py
@@ -162,8 +162,8 @@ def comment_to_GTD_count(comment):
 LABEL_DAILY_DICT = {
     # label, map_func, reduce_func
     "俯卧撑": [PUSHUP_LABEL_LIST, comment_to_int, sum],
-    # "冥想": [MEDITATION_LABEL_LIST, comment_to_int, sum],
-    # "早起": [MORNING_LABEL_LIST, commnet_to_count, len],  # Do Nothing
-    # "GTD": [GTD_LABEL_LIST, comment_to_GTD_count, sum],  # Do Nothing
+    "冥想": [MEDITATION_LABEL_LIST, comment_to_int, sum],
+    "早起": [MORNING_LABEL_LIST, commnet_to_count, len],  # Do Nothing
+    "GTD": [GTD_LABEL_LIST, comment_to_GTD_count, sum],  # Do Nothing
     "周记": [WEEKLY_LABEL_LIST, commnet_to_count, len],  # Do Nothing
 }
