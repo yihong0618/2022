7631c0a7735bade59348133e3e1a74d249407caf
Kilerd Chan <blove694@gmail.com>
2022-01-18 02:35:05 +0000

    feat: add clap entry

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use serde::Deserialize;
+use clap::{Parser, Args};
 
 static DEFAULT_SENTENCE: &str =
     "赏花归去马如飞\r\n去马如飞酒力微\r\n酒力微醒时已暮\r\n醒时已暮赏花归\r\n";
@@ -21,9 +22,39 @@ async fn get_one_sentence() -> Result<String> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let app_opts = AppOpts::parse();
+    match app_opts {
+        AppOpts::GetUp(get_up_opts) => {
+            get_up(get_up_opts).await
+        }
+    }
+    
+}
+
+async fn get_up(opts: GetUpOpts) -> Result<()> {
     let sentence = get_one_sentence()
-        .await
-        .unwrap_or(DEFAULT_SENTENCE.to_string());
+    .await
+    .unwrap_or(DEFAULT_SENTENCE.to_string());
     dbg!(sentence);
     Ok(())
+}
+
+
+#[derive(Debug, Parser)]
+#[clap(about, version, author)]
+enum AppOpts {
+    GetUp(GetUpOpts)
+}
+
+#[derive(Debug, Args)]
+struct GetUpOpts {
+    github_token: String,
+    repo_name: String,
+
+    /// custom weather message
+    wether_message: Option<String>,
+    /// telegram bot token
+    tele_token: Option<String>,
+    /// telegram chat id
+    tele_chat_id: Option<String>
 }
